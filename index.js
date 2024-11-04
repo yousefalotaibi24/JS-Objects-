0cdad8b19b98cb94541ed85d789431832860e0b3
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-11-04 19:27:02 +0300

    modifyed

---
+++ index.js
@@ -0,0 +1,24 @@
+//Part 1
+
+//Task 1
+const book = {
+  Title: "JavaScript: The Definitive Guide",
+  Author: "David Flanagan",
+  PublishedYear: 2020,
+  Genre: "Programming",
+};
+//Task 2
+console.log(book.Title);
+console.log(book["PublishedYear"]);
+
+//Part 2
+
+//Task 3
+
+book.pageCount = [1096];
+//Task 4
+book.ISBN = "978-1491952023";
+//Task 5
+book.PublishedYear = 2021;
+
+console.log(book);
